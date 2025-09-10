84545677c604f77c9044b2741214a509e1630225
DanikDaraboz <denchikgg2006@gmail.com>
2025-09-11 00:37:32 +0500

    Added localstorage for tasks and theme

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,18 +2,22 @@ import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 
 
 function App() {
-  const [tasks,setTasks] = useState([]);
+  const [tasks,setTasks] = useState(()=> {
+    const savedTasks = localStorage.getItem("tasks");
+    if (!savedTasks) return [];
+    return JSON.parse(savedTasks).map(task => ({...task, plannedDate: task.plannedDate ? new Date(task.plannedDate) : null}));
+  });
   const [notification,setNotification] = useState({show:false, message:""});
   const [showTask, setShowTask] = useState(false);
   const [editingIndex,setEditingIndex] = useState(null);
   const [editingTask,setEditingTask] = useState(null);
   const showTaskForm = ()=>{
     setShowTask(!showTask);
-
   }
 
   const timeRef = useRef(null);
@@ -25,6 +29,10 @@ function App() {
       setNotification({show:false, message});
     },2000);
   }
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <>
@@ -63,12 +71,24 @@ function App() {
 
 
 function Header({showTask, showTaskForm}) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(()=>{
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark"? true : false;
+  });
 
   const toggleTheme = ()=>{
     setDarkMode(!darkMode);
-    document.body.classList.toggle("dark-theme");
   }
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark-theme");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
 
   return (
     <header className={showTask?"blur":""}>
@@ -113,7 +133,7 @@ function TaskForm({tasks, setTasks, showNotification,showTask,setShowTask, editi
       setEditingTask(null);
     } else{
         const now = new Date();
-        const time = now.toLocaleString();
+        const time = now.toLocaleString();  
         const newTask ={
           text: input,
           time: time,
@@ -179,54 +199,64 @@ function TaskCard({tasks, setTasks, showNotification,showTask,setShowTask, setEd
     setShowTask(true);
   }
 
-  // const confirmChange = (index)=>{
-  //   if (changeInput == null || changeInput === "") return;
-  //   const updatedTasks = tasks.map((task,i)=>i===index?{...task,text:changeInput}:task);
-  //   setTasks(updatedTasks);
-  //   setEditingIndex(null);
-  //   setChangeInput("");
-  //   showNotification("Task updated successfully!");
-  // }
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+    const reorderedTasks = Array.from(tasks);
+    const [movedTask] = reorderedTasks.splice(result.source.index, 1);
+    reorderedTasks.splice(result.destination.index, 0, movedTask);
+    setTasks(reorderedTasks);
+  }
 
   return (
-    <ul id="task-cards" className={showTask?"blur":""}>
-      {tasks.map((task, index) => (
-          <li key={index} className={`task-list ${task.done ? "completed" : ""}`}>
-            
-            <hr style={{width:'100%'}} />
-            <div style={{display:'flex', gap:'10px'}}>
-            <span className='priority-dot' style={{
-              backgroundColor: 
-              task.priority === "High" ? "rgba(255, 0, 0, 0.1)" 
-              : task.priority === "Medium" ? "rgba(0, 0, 255, 0.1)" 
-              : task.priority === "Low" ? "rgba(0, 128, 0, 0.1)" 
-              : "transparent",
-              border:
-              task.priority === "High" ? "2.5px solid red"
-              : task.priority === "Medium" ? "2.5px solid blue"
-              : task.priority === "Low" ? "2.5px solid green"
-              : "none"
-            }}></span>
-              <div>
-                <div style={{fontSize:'1.1em'}}>{task.text}</div>
-                <div style={{fontSize:'0.8em'}} className='task-description'>{task.description}</div>
-                <div style={{ fontSize: '1em', color: '#888' }}>{task.plannedDate?.toLocaleDateString()}</div>
-                <div className='button-group'>
-                  <button className={`done-button ${task.done ? "completed" : ""}`} onClick={()=> taskDone(index)}>Done</button>
-                  <button id="delete-button" onClick={()=> deleteTask(index)}>Delete</button>
-                  <button id="update-button" onClick={()=> updateTask(index)}>Update</button>
-                </div>
-                <div className='task-meta'>
-                  <span className={`priority-indicator ${task.priority}`} style={{fontSize:'0.7em'}}>Priority: {task.priority} </span>
-                  <span style={{ marginLeft: '10px', fontSize: '0.7em', color: '#888' }}>Created on: ({task.time})</span>
-                </div>
-              </div>
-            </div>
-          </li>
-      ))}
-      {tasks.length > 0 && <hr style={{width:'100%'}} />}
-      {tasks.length === 0 && <p style={{textAlign:'center', color:'#888', marginTop:'20px'}}>No tasks available. Add a new task!</p>}
-    </ul>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="taskList">
+        {(provided) => (
+          <ul id="task-cards" className={showTask?"blur":""} ref={provided.innerRef} {...provided.droppableProps}>
+            {tasks.map((task, index) => (
+                <Draggable key={index} draggableId={`task-${index}`} index={index}>
+                  {(provided) => (          
+                    <li key={index} className={`task-list ${task.done ? "completed" : ""}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      
+                      <hr style={{width:'100%'}} />
+                      <div style={{display:'flex', gap:'10px'}}>
+                      <span className='priority-dot' style={{
+                        backgroundColor: 
+                        task.priority === "High" ? "rgba(255, 0, 0, 0.1)" 
+                        : task.priority === "Medium" ? "rgba(0, 0, 255, 0.1)" 
+                        : task.priority === "Low" ? "rgba(0, 128, 0, 0.1)" 
+                        : "transparent",
+                        border:
+                        task.priority === "High" ? "2.5px solid red"
+                        : task.priority === "Medium" ? "2.5px solid blue"
+                        : task.priority === "Low" ? "2.5px solid green"
+                        : "none"
+                      }}></span>
+                        <div>
+                          <div style={{fontSize:'1.1em'}}>{task.text}</div>
+                          <div style={{fontSize:'0.8em'}} className='task-description'>{task.description}</div>
+                          <div style={{ fontSize: '1em', color: '#888' }}>{task.plannedDate?.toLocaleDateString()}</div>
+                          <div className='button-group'>
+                            <button className={`done-button ${task.done ? "completed" : ""}`} onClick={()=> taskDone(index)}>Done</button>
+                            <button id="delete-button" onClick={()=> deleteTask(index)}>Delete</button>
+                            <button id="update-button" onClick={()=> updateTask(index)}>Update</button>
+                          </div>
+                          <div className='task-meta'>
+                            <span className={`priority-indicator ${task.priority}`} style={{fontSize:'0.7em'}}>Priority: {task.priority} </span>
+                            <span style={{ marginLeft: '10px', fontSize: '0.7em', color: '#888' }}>Created on: ({task.time})</span>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  )}
+                </Draggable>
+            ))}
+            {provided.placeholder}
+            {tasks.length > 0 && <hr style={{width:'100%'}} />}
+            {tasks.length === 0 && <p style={{textAlign:'center', color:'#888', marginTop:'20px'}}>No tasks available. Add a new task!</p>}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
 
